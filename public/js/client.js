1c1d8141694d0d6be43f7f1b9c4e562a56daa38f
Hyeongjin, Kim <1981_khj@naver.com>
2012-03-23 01:43:08 +0000

    first commit

---
+++ public/js/client.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    var socket = io.connect('http://vstr.herokuapp.com/');
+    //var socket = io.connect('http://vslab.hjkim.c9.io/');
+    
+    socket.on('drawImg', function(data) {
+        console.log("drawImg");
+        console.log(data);
+        $("#outPutImg").attr("src",data);
+    });
+});
